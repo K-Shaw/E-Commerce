a2c171603e0a0018f748890c7f0dc51f11228208
K-Shaw <codeit1020@gmail.com>
2025-05-18 01:49:32 +0530

    Update Code

--- sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
@@ -26,12 +27,12 @@ public class CategoryServiceImpl implements CategoryService{
     public String deleteCategory(Long categoryId) {
         Category category = null;
         for(Category x : categories){
-            if(x.getCategoryId() == categoryId){
+            if(Objects.equals(x.getCategoryId(), categoryId)){
                 category = x;
                 break;
             }
         }
-        if(category == null) return "catgeoryId: " + categoryId + " doesn't exist";
+        if(category == null) return "categoryId: " + categoryId + " doesn't exist";
         categories.remove(category);
         return "Category with categoryId: " + categoryId + " deleted successfully";
     }
